e79fcb426e92ab265dda5d963be66268599646ca
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-05-25 08:25:43 +0530

    Delete terraform directory

--- terraform/foundation/sig.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_shared_image_gallery" "sig" {
-
-  name                = var.gallery_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-}
--- terraform/foundation/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-subscription_id     = "e397652c-2118-4f8c-918d-90f1bdb9bc73"
-
-resource_group_name = "rg-canada-prod"
-
-gallery_name        = "canadaProdSIG"
-
-location            = "canadacentral"
